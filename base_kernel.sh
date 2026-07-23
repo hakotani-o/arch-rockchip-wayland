ead65bb5449b67b29394b0d99c0231466c0054cd
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-07-23 18:38:25 +0900

    Update base_kernel.sh

--- base_kernel.sh
+++ base_kernel.sh
@@ -3,7 +3,8 @@ set -eE
 set -x
 
 
-sudo apt install -y arch-install-scripts archlinux-keyring pacman-package-manager libarchive-tools systemd-container libalpm16
+sudo apt install -y arch-install-scripts archlinux-keyring pacman-package-manager libarchive-tools systemd-container libalpm13t64 
+# libalpm16
 sudo cp  etc/pacman.d/mirrorlist /etc/pacman.d
 sudo cp  -a keyrings /usr/share/pacman
 sudo cp  etc/pacman.d/mirrorlist /etc/pacman.d
