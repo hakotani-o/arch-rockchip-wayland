87cfce7e9b48dc95bbd30cfb94f94397cba99065
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-07-23 22:57:51 +0900

    Uncomment das-u-boot.sh for orangepi-5 config

--- control-main.sh
+++ control-main.sh
@@ -3,7 +3,7 @@ set -eE
 
 start_time=`date`
 
-	#./das-u-boot.sh orangepi-5-rk3588s_defconfig
+	./das-u-boot.sh orangepi-5-rk3588s_defconfig
 	./base_kernel.sh
 	./base_rootfs.sh
 	sudo ./disk_image.sh orangepi-5 rk3588s-orangepi-5
