90651804acfa9d4192837bd289e1ecc697de5389
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-07-29 13:07:22 +0900

    Update make-dot-config.sh

--- make-dot-config.sh
+++ make-dot-config.sh
@@ -17,22 +17,24 @@ sed -i '/conflicts=/d' ../../PKGBUILD
 sed -i 's/echo "Installing modules..."/mv "${pkgdir}\/boot\/Image.gz" "${pkgdir}\/boot\/vmlinuz-${kernver}"\n  rm "${pkgdir}\/boot\/Image" \n\n\n echo "Installing modules..."/' ../../PKGBUILD
 
 
-make defconfig
+#make defconfig
+cat ../../config > .config
   ./scripts/kconfig/merge_config.sh -m .config /home/builder/my-add.txt
   ./scripts/config --set-val DEBUG_INFO_NONE y
   ./scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
   ./scripts/config --disable DEBUG_INFO_DWARF4
   ./scripts/config --disable DEBUG_INFO_DWARF5
   make olddefconfig
-  ./scripts/diffconfig .config ../../config | grep "^ " | awk '$2 != "n"' | sed 's/-//' | awk '{ print "CONFIG_" $1 "=" $4 }' >> /home/builder/my-add.txt
-  rm .config
-  make defconfig
-  ./scripts/kconfig/merge_config.sh -m .config /home/builder/my-add.txt
-  ./scripts/config --set-val DEBUG_INFO_NONE y
-  ./scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
-  ./scripts/config --disable DEBUG_INFO_DWARF4
-  ./scripts/config --disable DEBUG_INFO_DWARF5
-  make olddefconfig
+  
+  #./scripts/diffconfig .config ../../config | grep "^ " | awk '$2 != "n"' | sed 's/-//' | awk '{ print "CONFIG_" $1 "=" $4 }' >> /home/builder/my-add.txt
+  #rm .config
+  #make defconfig
+  #./scripts/kconfig/merge_config.sh -m .config /home/builder/my-add.txt
+  #./scripts/config --set-val DEBUG_INFO_NONE y
+  #./scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
+  #./scripts/config --disable DEBUG_INFO_DWARF4
+  #./scripts/config --disable DEBUG_INFO_DWARF5
+  #make olddefconfig
 
   sed -i 's/CONFIG_LOCALVERSION="-ARCH"/CONFIG_LOCALVERSION=""/' .config
   cp .config ../../config
